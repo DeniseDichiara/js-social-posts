96aae3ad0ec9997c6b5502a45b9e4f0a508f90c4
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-31 17:19:15 +0200

    added class="post"

--- myscript.js
+++ myscript.js
@@ -6,18 +6,20 @@ console.log(postMeta);
 
 
 
-const containerElement = document.querySelector('div.posts-list');
-console.log(containerElement);
+//const containerElement = document.querySelector('div.posts-list');
+//console.log(containerElement);
 
-containerElement.innerHTML +=
+//containerElement.innerHTML +=
+
+postMeta.innerHTML +=
+`
+    <div class="post"></div>
 
 `
-<div class="post-meta__icon">
+
+/*<div class="post-meta__icon">
     <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
 </div>
-
-`
-
 
 /*<div class="post-meta__icon">
     <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
