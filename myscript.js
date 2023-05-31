be413b91ded64096024f5c7c59beb5bba08763b0
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-31 16:53:32 +0200

    innerhtml

---
+++ myscript.js
@@ -0,0 +1,19 @@
+
+
+const containerElement = document.querySelector('div.posts-list');
+console.log(containerElement);
+
+containerElement.innerHTML +=
+
+`
+<div class="post-meta__icon">
+    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+</div>
+
+`
+
+
+/*<div class="post-meta__icon">
+    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+</div>
+*/
