90cdf9f08abcbe1536636b5457bb90cb7cfd4f7b
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-01 11:18:08 +0200

    modified forEach and innerHTML

--- myscript.js
+++ myscript.js
@@ -66,25 +66,42 @@ const postElement = document.querySelector('div.posts-list');
 console.log(postElement);
 
 
-posts.forEach(( imageElement) => {
+posts.forEach(( post) => {
 
-    function addElement(){
-        const newPost = document.createElement('div');
-
-        const newContent = document.createTextNode('Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias');
-
-        newPost.append(newContent);
-
-        const currentDiv = document.getElementById('div.post');
-
-        document.body.insertBefore(newPost, currentDiv);
-    }
+    
 
     postElement.innerHTML +=
 `
-    <div id="container" class="posts-list">
-        <img src="${imageElement.media}" alt="mare">
+<div class="post">
+<div class="post__header">
+    <div class="post-meta">                    
+        <div class="post-meta__icon">
+            <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+        </div>
+        <div class="post-meta__data">
+            <div class="post-meta__author">${post.author.name}</div>
+            <div class="post-meta__time">${post.created}</div>
+        </div>                    
     </div>
+</div>
+<div class="post__text">${post.content}</div>
+<div class="post__image">
+    <img src="${post.media}" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+        </div>
+    </div> 
+</div>            
+</div>
 `
 })
 
